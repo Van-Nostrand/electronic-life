194c4ee68133e871841f40da167f22be769d2ed8
Van-Nostrand <dmdoull43@gmail.com>
2021-12-12 18:59:34 -0800

    added linting, upgraded package.json

--- src/constants/CONSTANTS.js
+++ src/constants/CONSTANTS.js
@@ -1,33 +1,33 @@
 export const DEFAULT_PLAN = [
-  "############################",
-  "#      #b   #             ##",
-  "#      #    #              #",
-  "#      ##                  #",
-  "#                          #",
-  "#        ######            #",
-  "#             #            #",
-  "#             #####        #",
-  "#    b                     #",
-  "#                          #",
-  "#                          #",
-  "#                    #     #",
-  "#     ##      b      #     #",
-  "#     ##         #   #     #",
-  "#    ###          ####     #",
-  "#   ## #                   #",
-  "#   #   #           b      #",
-  "#   #                      #",
-  "#                          #",
-  "#          #####           #",
-  "##         #   #    ##     #",
-  "###           ##     #     #",
-  "#           ###      #     #",
-  "#   ####                   #",
-  "#   ##       b             #",
-  "# b  #         b       ### #",
-  "#    #                     #",
-  "############################"
-];
+  '############################',
+  '#      #b   #             ##',
+  '#      #    #              #',
+  '#      ##                  #',
+  '#                          #',
+  '#        ######            #',
+  '#             #            #',
+  '#             #####        #',
+  '#    b                     #',
+  '#                          #',
+  '#                          #',
+  '#                    #     #',
+  '#     ##      b      #     #',
+  '#     ##         #   #     #',
+  '#    ###          ####     #',
+  '#   ## #                   #',
+  '#   #   #           b      #',
+  '#   #                      #',
+  '#                          #',
+  '#          #####           #',
+  '##         #   #    ##     #',
+  '###           ##     #     #',
+  '#           ###      #     #',
+  '#   ####                   #',
+  '#   ##       b             #',
+  '# b  #         b       ### #',
+  '#    #                     #',
+  '############################'
+]
 // export const DEFAULT_PLAN = [
 //   "##########",
 //   "#        #",
@@ -61,19 +61,19 @@ export const DEFAULT_PLAN = [
 //   "#####"
 // ];
 
-export const ALL_CRITTER_TYPES = ["b","w","p"];
+export const ALL_CRITTER_TYPES = ['b', 'w', 'p']
 
-export const DIRECTION_NAMES = "n ne e se s sw w nw".split(" ");
+export const DIRECTION_NAMES = 'n ne e se s sw w nw'.split(' ')
 
 export const DIRECTION_BY_STRING = {
-  "n": { x: 0, y: -1 }, //n
-  "ne": { x: 1, y: -1 }, //ne
-  "e": { x: 1, y: 0 },  //e
-  "se": { x: 1, y: 1 },  //se
-  "s": { x: 0, y: 1 },  //s
-  "sw": { x: -1, y: 1 },//sw
-  "w": { x: -1, y: 0 }, //w
-  "nw": { x: -1, y: -1 } //nw
+  'n': { x: 0, y: -1 }, //n
+  'ne': { x: 1, y: -1 }, //ne
+  'e': { x: 1, y: 0 },  //e
+  'se': { x: 1, y: 1 },  //se
+  's': { x: 0, y: 1 },  //s
+  'sw': { x: -1, y: 1 }, //sw
+  'w': { x: -1, y: 0 }, //w
+  'nw': { x: -1, y: -1 } //nw
 }
 
 export const DIRECTIONS = [
@@ -85,65 +85,65 @@ export const DIRECTIONS = [
   { x: -1, y: 1 }, //sw
   { x: -1, y: 0 }, //w
   { x: -1, y: -1 } //nw
-];
+]
 
 export const CARDINAL_STRING_FROM_COORDINATE = {
-  "0,-1": "n",
-  "1,-1": "ne",
-  "1,0": "e",
-  "1,1": "se",
-  "0,1": "s",
-  "-1,1": "sw",
-  "-1,0": "w",
-  "-1,-1": "nw"
+  '0,-1': 'n',
+  '1,-1': 'ne',
+  '1,0': 'e',
+  '1,1': 'se',
+  '0,1': 's',
+  '-1,1': 'sw',
+  '-1,0': 'w',
+  '-1,-1': 'nw'
 }
 
 export const getCardinalString = (x, y) => {
   if (x === 0) {
-    if (y === 1) return "s";
-    else return "n";
+    if (y === 1) return 's'
+    else return 'n'
   }
   else if (x === 1) {
-    if (y === 0) return "e";
-    else if (y === 1) return "se";
-    else if (y === -1) return "ne";
+    if (y === 0) return 'e'
+    else if (y === 1) return 'se'
+    else if (y === -1) return 'ne'
   }
   else if (x === -1) {
-    if (y === 0) return "w";
-    else if (y === 1) return "sw";
-    else if (y === -1) return "nw";
+    if (y === 0) return 'w'
+    else if (y === 1) return 'sw'
+    else if (y === -1) return 'nw'
   }
 }
 
 //this creates an array of length 8 representing 8 directions of travel
 //each index is either true or false, representing if a creature can move there
-export function creatureViews(creatureArray, gridArray) {
+export function creatureViews (creatureArray, gridArray) {
   creatureArray.forEach((creature, i) => {
-    let surroundingArea = new Array(8).fill();
-  
+    const surroundingArea = new Array(8).fill()
+
     surroundingArea.map((cell, n) => {
-      let cellLocation = [creature.x + DIRECTIONS[n].x, creature.y + DIRECTIONS[n].y];
-      let j = creatureArray.length - 1;
-      for(let c = i + 1; c < j; c++){
+      const cellLocation = [creature.x + DIRECTIONS[n].x, creature.y + DIRECTIONS[n].y]
+      const j = creatureArray.length - 1
+      for (let c = i + 1; c < j; c++) {
         // if not occupied by creature - IGNORE FOR NOW
         // if((creatureArray[c].x === cellLocation[0])&&(creatureArray[c].y === cellLocation[1])) return false;
         // if not a wall
-        if(gridArray[cellLocation[0]][cellLocation[1]] === "#" ) return false;
+        if (gridArray[cellLocation[0]][cellLocation[1]] === '#' ) return false
       }
-      return true;
-    });
-    creatureArray[i].view = surroundingArea;
-  });
+      return true
+    })
+    creatureArray[i].view = surroundingArea
+  })
 }
 
 //update the grid and creatures arrays
-export function updateGrid(newCreatures, newGrid) {
+export function updateGrid (newCreatures, newGrid) {
   newCreatures.forEach((creature, i) => {
-    newGrid[creature.x + creature.facing.x][creature.y + creature.facing.y] = creaturecritterType;
-    newGrid[creature.x][creature.y] = " ";
-    creature.x = creature.x + creature.facing.x;
-    creature.y = creature.y + creature.facing.y;
-  });
+    newGrid[creature.x + creature.facing.x][creature.y + creature.facing.y] = creaturecritterType
+    newGrid[creature.x][creature.y] = ' '
+    creature.x = creature.x + creature.facing.x
+    creature.y = creature.y + creature.facing.y
+  })
 
-  return newGrid;
+  return newGrid
 }
