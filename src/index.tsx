c33aa3d6b22f6b321561ccebc397bed41b30f415
Daniel Doull <dmdoull43@gmail.com>
2023-01-20 12:46:17 -0800

    running properly again

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import { createRoot } from 'react-dom/client'
 import ElectronicLife from './ElectronicLife'
 
-ReactDOM.render(<ElectronicLife />, document.getElementById('root'))
+const root = createRoot(document.getElementById('root'))
+root.render(<ElectronicLife />)
--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,19 @@
 const HtmlWebPackPlugin = require('html-webpack-plugin')
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const path = require('path')
+const isProd = process.env.NODE_ENV === 'production'
 
-module.exports = {
-  entry: __dirname + '/src/index.ts',
+const config = {
+  mode: isProd ? 'production' : 'development',
+  context: __dirname,
+  entry: {
+    index: '/src/index.tsx'
+  },
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
-    publicPath: ''
+    filename: './bundle.js'
+    // publicPath: ''
   },
   module: {
     rules: [
@@ -24,14 +30,27 @@ module.exports = {
         exclude: /node_modules/
       },
       {
+        test: /\.(jpe?g|svg|png|gif)$/,
+        type: 'asset/resource'
+      },
+      {
+        test: /\.(woff(2)?|ttf|eot)(\?v=\d+\.\d+\.\d+)?$/,
+        type: 'asset/resource'
+      },
+      {
         test: /\.css$/,
         use: [MiniCssExtractPlugin.loader, 'css-loader']
       }
     ]
   },
   resolve: {
-    extensions: ['.tsx', '.ts', '.js']
+    extensions: ['.tsx', '.ts', '.js', '.css', '.scss'],
+    alias: {
+      '@': path.resolve(__dirname, 'src/')
+    }
   },
+  devtool: 'inline-source-map',
+
   plugins: [
     new HtmlWebPackPlugin({
       template: path.resolve(__dirname, 'public/index.html'),
@@ -40,6 +59,19 @@ module.exports = {
     new MiniCssExtractPlugin({
       filename: '[name].css',
       chunkFilename: '[id].css'
-    })
+    }),
+    new ForkTsCheckerWebpackPlugin()
   ]
 }
+
+if (!isProd) {
+  config.devServer = {
+    port: 8080,
+    open: false,
+    hot: true,
+    compress: true,
+    historyApiFallback: true
+  }
+}
+
+module.exports = config
